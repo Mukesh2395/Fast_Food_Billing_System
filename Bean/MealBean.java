de67911a2f25cf16ec3ad5971de4780aaf75f73f
Mukesh2395 <38472061+Mukesh2395@users.noreply.github.com>
2018-04-22 22:10:29 +0530

    Add files via upload

---
+++ Bean/MealBean.java
@@ -0,0 +1,71 @@
+
+package Bean;
+
+public class MealBean {
+
+    public static String getFries() {
+        return fries;
+    }
+
+    public static String getSalad() {
+        return salad;
+    }
+
+    public static String getHamburger() {
+        return hamburger;
+    }
+
+    public static String getOnionrings() {
+        return onionrings;
+    }
+
+    public static String getChickensalad() {
+        return chickensalad;
+    }
+
+    public static String getFish_sandwich() {
+        return fish_sandwich;
+    }
+
+    public static String getChicken_sandwich() {
+        return chicken_sandwich;
+    }
+
+    public static String getCheese_sandwich() {
+        return cheese_sandwich;
+    }
+
+    public static void setFries(String fries) {
+        MealBean.fries = fries;
+    }
+
+    public static void setSalad(String salad) {
+        MealBean.salad = salad;
+    }
+
+    public static void setHamburger(String hamburger) {
+        MealBean.hamburger = hamburger;
+    }
+
+    public static void setOnionrings(String onionrings) {
+        MealBean.onionrings = onionrings;
+    }
+
+    public static void setChickensalad(String chickensalad) {
+        MealBean.chickensalad = chickensalad;
+    }
+
+    public static void setFish_sandwich(String fish_sandwich) {
+        MealBean.fish_sandwich = fish_sandwich;
+    }
+
+    public static void setChicken_sandwich(String chicken_sandwich) {
+        MealBean.chicken_sandwich = chicken_sandwich;
+    }
+
+    public static void setCheese_sandwich(String cheese_sandwich) {
+        MealBean.cheese_sandwich = cheese_sandwich;
+    }
+   public static String fries,salad,hamburger,onionrings,chickensalad,fish_sandwich,chicken_sandwich,cheese_sandwich;
+    
+}
