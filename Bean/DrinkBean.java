47c705c9401f8798fe2bcfe5348fe960eb5a907c
Mukesh2395 <38472061+Mukesh2395@users.noreply.github.com>
2018-04-22 22:09:23 +0530

    Add files via upload

---
+++ Bean/DrinkBean.java
@@ -0,0 +1,64 @@
+
+package Bean;
+
+public class DrinkBean {
+public static String coca_cola,pepsi,Diet_coak,mountain_dew,fanta,maza,redbull;
+
+    public static String getCoca_cola() {
+        return coca_cola;
+    }
+
+    public static void setCoca_cola(String coca_cola) {
+        DrinkBean.coca_cola = coca_cola;
+    }
+
+    public static void setPepsi(String pepsi) {
+        DrinkBean.pepsi = pepsi;
+    }
+
+    public static void setDiet_coak(String Diet_coak) {
+        DrinkBean.Diet_coak = Diet_coak;
+    }
+
+    public static void setMountain_dew(String mountain_dew) {
+        DrinkBean.mountain_dew = mountain_dew;
+    }
+
+    public static void setFanta(String fanta) {
+        DrinkBean.fanta = fanta;
+    }
+
+    public static void setMaza(String maza) {
+        DrinkBean.maza = maza;
+    }
+
+    public static void setRedbull(String redbull) {
+        DrinkBean.redbull = redbull;
+    }
+
+    public static String getPepsi() {
+        return pepsi;
+    }
+
+    public static String getDiet_coak() {
+        return Diet_coak;
+    }
+
+    public static String getMountain_dew() {
+        return mountain_dew;
+    }
+
+    public static String getFanta() {
+        return fanta;
+    }
+
+    public static String getMaza() {
+        return maza;
+    }
+
+    public static String getRedbull() {
+        return redbull;
+    }
+
+   
+}
