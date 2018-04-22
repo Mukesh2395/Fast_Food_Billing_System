57cca190fb55d5879a463fe95b1f07cdc02bd079
Mukesh2395 <38472061+Mukesh2395@users.noreply.github.com>
2018-04-22 22:11:24 +0530

    Add files via upload

---
+++ Bean/TeaBean.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bean;
+
+public class TeaBean {
+    public static String brown_tea,black_tea,ice_tea,green_tea,ginger_tea,lemon_Tea;
+
+    public static String getBrown_tea() {
+        return brown_tea;
+    }
+
+    public static void setBrown_tea(String brown_tea) {
+        TeaBean.brown_tea = brown_tea;
+    }
+
+    public static void setBlack_tea(String black_tea) {
+        TeaBean.black_tea = black_tea;
+    }
+
+    public static void setIce_tea(String ice_tea) {
+        TeaBean.ice_tea = ice_tea;
+    }
+
+    public static void setGreen_tea(String green_tea) {
+        TeaBean.green_tea = green_tea;
+    }
+
+    public static void setGinger_tea(String ginger_tea) {
+        TeaBean.ginger_tea = ginger_tea;
+    }
+
+    public static void setLemon_Tea(String lemon_Tea) {
+        TeaBean.lemon_Tea = lemon_Tea;
+    }
+
+    public static String getBlack_tea() {
+        return black_tea;
+    }
+
+    public static String getIce_tea() {
+        return ice_tea;
+    }
+
+    public static String getGreen_tea() {
+        return green_tea;
+    }
+
+    public static String getGinger_tea() {
+        return ginger_tea;
+    }
+
+    public static String getLemon_Tea() {
+        return lemon_Tea;
+    }
+    
+}
